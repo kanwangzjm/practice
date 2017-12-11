28bac026e126d5f578a0fcfc5ad6bd3cfbd4f1de
jimin.zheng <kanwangzjm@gmail.com>
2017-12-11 23:26:12 +0800

    init

---
+++ src/main/java/com/mmall/practice/PracticeApplication.java
@@ -0,0 +1,12 @@
+package com.mmall.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeApplication.class, args);
+    }
+}
---
+++ src/main/java/com/mmall/practice/annoations/NotRecommend.java
@@ -0,0 +1,8 @@
+package com.mmall.practice.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+public @interface NotRecommend {
+}
