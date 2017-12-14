1a93ebf3acdde0f91dd6317efe8334138a80c501
jimin.zheng <kanwangzjm@gmail.com>
2017-12-15 00:33:30 +0800

    add hystrix dashboard

--- src/main/java/com/mmall/practice/PracticeApplication.java
+++ src/main/java/com/mmall/practice/PracticeApplication.java
@@ -2,8 +2,12 @@ package com.mmall.practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class PracticeApplication {
 
     public static void main(String[] args) {
