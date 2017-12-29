50ce5cc1cd259853219170d2b3531f524acdfa4f
jimin.zheng <kanwangzjm@gmail.com>
2017-12-29 17:48:06 +0800

    add ForkJoin

---
+++ src/main/java/com/mmall/practice/example/aqs/ForkJoinExample1.java
@@ -0,0 +1,68 @@
+package com.mmall.practice.example.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
+import java.util.concurrent.RecursiveTask;
+
+// http://www.infoq.com/cn/articles/fork-join-introduction
+// https://www.cnblogs.com/jiyuqi/p/4547082.html
+@Slf4j
+public class ForkJoinExample1 extends RecursiveTask<Integer> {
+
+    public static final int threshold = 2;
+    private int start;
+    private int end;
+
+    public ForkJoinExample1(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected Integer compute() {
+        int sum = 0;
+
+        //如果任务足够小就计算任务
+        boolean canCompute = (end - start) <= threshold;
+        if (canCompute) {
+            for (int i = start; i <= end; i++) {
+                sum += i;
+            }
+        } else {
+            // 如果任务大于阈值，就分裂成两个子任务计算
+            int middle = (start + end) / 2;
+            ForkJoinExample1 leftTask = new ForkJoinExample1(start, middle);
+            ForkJoinExample1 rightTask = new ForkJoinExample1(middle + 1, end);
+
+            // 执行子任务
+            leftTask.fork();
+            rightTask.fork();
+
+            //等待任务执行结束合并其结果
+            int leftResult = leftTask.join();
+            int rightResult = rightTask.join();
+
+            //合并子任务
+            sum = leftResult + rightResult;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        ForkJoinPool forkjoinPool = new ForkJoinPool();
+
+        //生成一个计算任务，计算1+2+3+4
+        ForkJoinExample1 task = new ForkJoinExample1(1, 100);
+
+        //执行一个任务
+        Future<Integer> result = forkjoinPool.submit(task);
+
+        try {
+            log.info("result:{}", result.get());
+        } catch (Exception e) {
+            log.error("exception", e);
+        }
+    }
+}
