4ecd566e5fc76d156d87aa4058cb5f15318b6285
jimin.zheng <kanwangzjm@gmail.com>
2018-01-02 22:08:24 +0800

    add CyclicBarrier

---
+++ src/main/java/com/mmall/practice/example/aqs/CyclicBarrierExample1.java
@@ -0,0 +1,52 @@
+package com.mmall.practice.example.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * 通过它可以实现让一组线程等待至某个状态之后再全部同时执行。叫做回环是因为当所有等待线程都被释放以后，CyclicBarrier可以被重用。
+ * 我们暂且把这个状态就叫做barrier，当调用await()方法之后，线程就处于barrier了
+ *
+ * CyclicBarrier可以用于多线程计算数据，最后合并计算结果的应用场景。
+ * 比如我们用一个Excel保存了用户所有银行流水，每个Sheet保存一个帐户近一年的每笔银行流水，
+ * 现在需要统计用户的日均银行流水，先用多线程处理每个sheet里的银行流水，
+ * 都执行完之后，得到每个sheet的日均银行流水，
+ * 最后，再用barrierAction用这些线程的计算结果，计算出整个Excel的日均银行流水
+ *
+ * CyclicBarrier和CountDownLatch的区别
+ * CountDownLatch的计数器只能使用一次。
+ * 而CyclicBarrier的计数器可以使用reset() 方法重置。
+ * 所以CyclicBarrier能处理更为复杂的业务场景，比如如果计算发生错误，可以重置计数器，并让线程们重新执行一次。
+ * CyclicBarrier还提供其他有用的方法，比如getNumberWaiting方法可以获得CyclicBarrier阻塞的线程数量。
+ * isBroken方法用来知道阻塞的线程是否被中断。
+ */
+@Slf4j
+public class CyclicBarrierExample1 {
+
+    private CyclicBarrier barrier = new CyclicBarrier(5, () -> {
+        // 在线程到达屏障时，优先执行部分
+//        log.info("Callback is running");
+    });
+
+    public void race() throws Exception {
+        log.info("Thread " + Thread.currentThread().getName() + " is waiting the resource");
+        barrier.await();
+        log.info("Thread " + Thread.currentThread().getName() + " got the resource");
+    }
+
+    public static void main(String[] args) {
+        final CyclicBarrierExample1 cyclicBarrierExample1 = new CyclicBarrierExample1();
+
+        for (int i = 0; i < 12; i++) {
+            new Thread(() -> {
+                try {
+                    cyclicBarrierExample1.race();
+                } catch (Exception e) {
+                    log.error("exception", e);
+                }
+
+            }, String.valueOf(i)).start();
+        }
+    }
+}
