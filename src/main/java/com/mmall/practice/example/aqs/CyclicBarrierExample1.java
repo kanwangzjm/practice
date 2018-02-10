3bf8b4803904477022e2ee5c15b5ac9f137a91c0
jimin.zheng <kanwangzjm@gmail.com>
2018-02-10 18:41:19 +0800

    update

--- src/main/java/com/mmall/practice/example/aqs/CyclicBarrierExample1.java
+++ src/main/java/com/mmall/practice/example/aqs/CyclicBarrierExample1.java
@@ -2,20 +2,22 @@ package com.mmall.practice.example.aqs;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 通过它可以实现让一组线程等待至某个状态之后再全部同时执行。叫做回环是因为当所有等待线程都被释放以后，CyclicBarrier可以被重用。
  * 我们暂且把这个状态就叫做barrier，当调用await()方法之后，线程就处于barrier了
- *
+ * <p>
  * CyclicBarrier可以用于多线程计算数据，最后合并计算结果的应用场景。
  * 比如我们用一个Excel保存了用户所有银行流水，每个Sheet保存一个帐户近一年的每笔银行流水，
  * 现在需要统计用户的日均银行流水，先用多线程处理每个sheet里的银行流水，
  * 都执行完之后，得到每个sheet的日均银行流水，
  * 最后，再用barrierAction用这些线程的计算结果，计算出整个Excel的日均银行流水
- *
+ * <p>
  * CyclicBarrier和CountDownLatch的区别
  * CountDownLatch的计数器只能使用一次。
  * 而CyclicBarrier的计数器可以使用reset() 方法重置。
@@ -26,29 +28,33 @@ import java.util.concurrent.Executors;
 @Slf4j
 public class CyclicBarrierExample1 {
 
-    private CyclicBarrier barrier = new CyclicBarrier(5, () -> {
+    private static CyclicBarrier barrier = new CyclicBarrier(5, () -> {
         // 在线程到达屏障时，优先执行部分
 //        log.info("Callback is running");
     });
 
-    public void race(int threadNum) throws Exception {
+    public static void race(int threadNum) throws Exception {
         Thread.sleep(1000);
         log.info("{} is ready", threadNum);
-        barrier.await();
+        try {
+            barrier.await(2000, TimeUnit.MILLISECONDS);
+        } catch (BrokenBarrierException e) {
+            log.warn("BrokenBarrierException", e);
+        }
+
         log.info("{} continue", threadNum);
     }
 
     public static void main(String[] args) throws Exception {
 
         ExecutorService exec = Executors.newCachedThreadPool();
-        final CyclicBarrierExample1 cyclicBarrierExample1 = new CyclicBarrierExample1();
 
         for (int i = 0; i < 10; i++) {
             final int threadNum = i;
             Thread.sleep(1000);
             exec.execute(() -> {
                 try {
-                    cyclicBarrierExample1.race(threadNum);
+                    race(threadNum);
                 } catch (Exception e) {
                     log.error("exception", e);
                 }
