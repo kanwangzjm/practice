08238e5e0ab65339cec254b01517320ff622fbdc
jimin.zheng <kanwangzjm@gmail.com>
2018-03-11 16:15:34 +0800

    update mq

--- src/main/java/com/mmall/practice/example/mq/kafka/KafKaReceiver.java
+++ src/main/java/com/mmall/practice/example/mq/kafka/KafKaReceiver.java
@@ -5,20 +5,12 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
-
 @Component
 @Slf4j
 public class KafKaReceiver {
 
     @KafkaListener(topics = {TopicConstants.TEST})
     public void receive(ConsumerRecord<?, ?> record) {
-        Optional<?> kafkaMessage = Optional.ofNullable(record);
-        if (kafkaMessage.isPresent()) {
-            Object message = kafkaMessage.get();
-
-            log.info("record:{}", record);
-            log.info("message:{}", message);
-        }
+        log.info("record:{}", record);
     }
 }
