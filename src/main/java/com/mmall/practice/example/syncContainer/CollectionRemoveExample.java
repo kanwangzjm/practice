84c54f95f1146e91a38152a39dc3a1bdafc6ce1c
jimin.zheng <kanwangzjm@gmail.com>
2018-05-19 12:33:45 +0800

    add collection remove

---
+++ src/main/java/com/mmall/practice/example/syncContainer/CollectionRemoveExample.java
@@ -0,0 +1,59 @@
+package com.mmall.practice.example.syncContainer;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+public class CollectionRemoveExample {
+
+    // java.util.ConcurrentModificationException
+    private static void test1(Vector<Integer> v1) { // foreach
+        for(Integer i : v1) {
+            if (i.equals(3)) {
+                v1.remove(i);
+            }
+        }
+    }
+
+    // java.util.ConcurrentModificationException
+    private static void test2(Vector<Integer> v1) { // iterator
+        Iterator<Integer> iterator = v1.iterator();
+        while (iterator.hasNext()) {
+            Integer i = iterator.next();
+            if (i.equals(3)) {
+                v1.remove(i);
+            }
+        }
+    }
+
+    // success
+    private static void test3(Vector<Integer> v1) { // for
+        for (int i = 0; i < v1.size(); i++) {
+            if (v1.get(i).equals(3)) {
+                v1.remove(i);
+            }
+        }
+    }
+
+    // success
+    private static void test4(Vector<Integer> v1) { // iterator
+        Iterator<Integer> iterator = v1.iterator();
+        while (iterator.hasNext()) {
+            Integer i = iterator.next();
+            if (i.equals(3)) {
+                iterator.remove();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Vector<Integer> vector = new Vector<>();
+        vector.add(1);
+        vector.add(2);
+        vector.add(3);
+        vector.add(4);
+        vector.add(5);
+        test3(vector);
+    }
+
+}
